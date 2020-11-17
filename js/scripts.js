59c6188ff43dd751f99773eb3ec245792439efee
YABAD00 <jyab0617@gmail.com>
2020-11-16 19:30:35 -0500

    add scrolltrigger positions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#row1", {
+gsap.to(".row1", {
   scrollTrigger: {
+    trigger: ".row1",
+    start: "top 100%",
+    end: "bottom 0%",
+    markers: true,
     scrub: true
   },
   y: (i, target) => -ScrollTrigger.maxScroll(window) * 
