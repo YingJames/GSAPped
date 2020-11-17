f7649a58772aa82aae39672385018b6924bd2e0f
YABAD00 <jyab0617@gmail.com>
2020-11-17 13:02:35 -0500

    fix parallax

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,63 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
+// function initParallax() {
+//   gsap.utils.toArray(".col2 .parallax").forEach((section, i) => {
+//     const heightDiff = section.offsetHeight - section.parentElement.offsetHeight;
+    
+//     gsap.fromTo(section,{ 
+//       y: -heightDiff
+//     }, {
+//       scrollTrigger: {
+//         trigger: section,
+//         scrub: true
+//       },
+//       y: 0,
+//       ease: "none"
+//     });
+//   });
 
-gsap.to(".row1", {
-  scrollTrigger: {
-    trigger: ".row1",
-    start: "top 100%",
-    end: "bottom 0%",
-    markers: true,
-    scrub: true
-  },
-  y: (i, target) => -ScrollTrigger.maxScroll(window) * 
-target.dataset.speed,
-  ease: "none"
 
+// }
+
+// function initPage() {
+//   initParallax();
+// }
+
+// initPage();
+
+
+gsap.utils.toArray(".col--2").forEach((section, i) => {
+  section.bg = section.querySelector(".bg"); 
+
+  // Give the backgrounds some random images
+
+  // Do the parallax effect on each section
+  if (i) {
+    section.bg.style.backgroundPosition = `50% ${-innerHeight / 2}px`;
+
+    gsap.to(section.bg, {
+      backgroundPosition: `50% ${innerHeight / 2}px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        markers: true,
+        scrub: true
+      }
+    });
+  } 
+  
+  // the first image should be positioned against the top. Use px on the animating part to work with GSAP. 
+  else {
+    section.bg.style.backgroundPosition = "50% 0px"; 
+
+    gsap.to(section.bg, {
+      backgroundPosition: `50% ${innerHeight / 2}px`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: section,
+        start: "top bottom", 
+        end: "bottom top",
+        scrub: true
+      }
+    });
+  }
 });
