6d350b5745b61f6eea8bb37d84a52321ad4a1096
YABAD00 <jyab0617@gmail.com>
2020-11-17 20:37:42 -0500

    bruh too much

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,75 @@
-gsap.utils.toArray(".col--2").forEach((section, i) => {
-  section.bg = section.querySelector(".bg"); 
+gsap.registerPlugin(ScrollTrigger);
 
-  // Give the backgrounds some random images
+function initParallax() {
+  gsap.utils.toArray(".col--2").forEach((section, i) => {
+    section.bg = section.querySelector(".bg");
 
-  // Do the parallax effect on each section
-  if (i) {
-    section.bg.style.backgroundPosition = `50% ${-innerHeight / 2}px`;
+    // Give the backgrounds some random images
 
-    gsap.to(section.bg, {
-      backgroundPosition: `50% ${innerHeight / 2}px`,
-      ease: "none",
-      scrollTrigger: {
-        trigger: section,
-        markers: true,
-        scrub: true
-      }
-    });
-  } 
-  
-  // the first image should be positioned against the top. Use px on the animating part to work with GSAP. 
-  else {
-    section.bg.style.backgroundPosition = "50% 0px"; 
+    // Do the parallax effect on each section
+    if (i) {
+      section.bg.style.backgroundPosition = `50% ${-innerHeight / 2}px`;
 
-    gsap.to(section.bg, {
-      backgroundPosition: `50% ${innerHeight / 2}px`,
-      ease: "none",
-      scrollTrigger: {
-        trigger: section,
-        start: "top bottom", 
-        end: "bottom top",
-        scrub: true
-      }
-    });
-  }
-});
+      gsap.to(section.bg, {
+        backgroundPosition: `50% ${innerHeight / 2}px`,
+        ease: "none",
+        scrollTrigger: {
+          trigger: section,
+          scrub: true,
+        },
+      });
+    }
+
+    // the first image should be positioned against the top. Use px on the animating part to work with GSAP.
+    else {
+      section.bg.style.backgroundPosition = "50% 0px";
+
+      gsap.to(section.bg, {
+        backgroundPosition: `50% ${innerHeight / 2}px`,
+        ease: "none",
+        scrollTrigger: {
+          trigger: section,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: true,
+        },
+      });
+    }
+  });
+}
+
+function initAnimation() {
+  let tl = gsap.timeline();
+  // gsap.from("#intro--title", {
+  //   x: -100,
+  //   opacity: 0,
+  //   ease: "power4",
+  //   duration: 3,
+  // });
+  gsap.to("#intro--title", {
+    duration: 2,
+    x: -300,
+    duration: 2,
+    scrollTrigger: {
+      trigger: "#intro",
+      start: "bottom 90%",
+      end: "bottom top",
+      markers: true,
+      scrub: true,
+    },
+  });
+}
+
+// function initSnap() {
+//   gsap.to(".col"),
+//     {
+//       snap: {
+//         snapTo: 0.5, // 0.5 'cause the scroll animation range is 200vh for parallax effect
+//         duration: 1,
+//         ease: "power4.inOut",
+//       },
+//     };
+// }
+
+initParallax();
+initAnimation();
