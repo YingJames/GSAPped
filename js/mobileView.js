944f346f82f1e32d7e20710cb53d590360948aa5
YABAD00 <jyab0617@gmail.com>
2020-11-21 11:25:30 -0500

    delete mobileView.js

--- js/mobileView.js
+++
@@ -1,3 +0,0 @@
-let height = window.innerHeight;
-document.documentElement.style.setProperty("--mobile-height", height + "px");
-console.log(document.documentElement.style.getPropertyValue("--mobile-height"));
