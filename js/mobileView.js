b5a6f4a0566c2b75a8bc97a8bd087855639d1dbd
YABAD00 <jyab0617@gmail.com>
2020-11-21 10:54:50 -0500

    add height based on mobile height

--- js/mobileView.js
+++ js/mobileView.js
@@ -1,2 +1,3 @@
 let height = window.innerHeight;
-console.log(height);
+document.documentElement.style.setProperty("--height", height + "px");
+console.log(document.documentElement.style.getPropertyValue("--height"));
